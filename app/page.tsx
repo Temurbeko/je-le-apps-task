9797a4c9710d55014a27c00be97e31cb1516b585
Temur <temurbekUnical@gmail.com>
2025-03-07 15:32:09 +0500

    bg white

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-white!">
       <div className="w-48 h-48 bg-black"></div>
     </div>
   );
